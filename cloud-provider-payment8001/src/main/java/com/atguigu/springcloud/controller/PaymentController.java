ea6ab4f0156234f5b963eea03230ad4061d09375
xujian-xj <2351704779@qq.com>
2021-11-03 00:02:24 +0800

    commit test

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -58,7 +58,6 @@ public class PaymentController {
         for (ServiceInstance instance : instances ){
             log.info(instance.getInstanceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
         }
-
         return discoveryClient;
     }
 
